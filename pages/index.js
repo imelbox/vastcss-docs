1ea06c769123ac6ec80fafa0f15d33f09833ad79
Mohamed Tamsamani <tamsmoha@gmail.com>
2020-03-08 20:07:10 +0100

    add initial install of nextjs & vastcss depenadant

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function HomePage() {
+  return (
+    <div>
+      <div className="text-size-3">
+        Welcome to <span className="text-big">npm i vastcss</span>x
+      </div>
+      <div className="text-size-2">
+        A nice UI with fully managed by
+        <pre>
+          <code className="text-small">css variables</code>
+        </pre>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
